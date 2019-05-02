a00f93c73f5befb962a364d8331a8521ce48b8b1
vstvr <383588293@qq.com>
2019-05-02 09:22:29 +0800

    2019-5-2
    GUI for main file

--- test.py
+++ test.py
@@ -1,21 +1,112 @@
-import os
-from searchword import *
 
-path = os.getcwd() + '/notebook/' + 'word.txt'
-autopath=os.getcwd()+'/auto/'+'autotranslate.txt'
+'''
+//hashlib
+import hashlib
 
-wordlist=[]
-fp = open(path, 'r')
-wordlist = fp.readlines()
-fp.close()
+a= hashlib.new('md5', b'what you are fucking about').hexdigest()
+print(a)
+'''
 
-translatelist=[]
-for i in wordlist:
-    words=i.split('——')
-    translatelist.append(words[0])
-del(translatelist[0])
+'''
+def func():
+    yield 1
+    yield 2
+    yield 3
 
-fp=open(autopath,'w')
-for i in translatelist:
-    fp.write(i+'——'+translate(i)+'\n')
-fp.close()
+f=func()
+print(f.send(4))
+'''
+
+'''
+//map
+def f(x,y):return [x,y]
+
+l1=[1,2,3,4,5,6]
+l2=['one','two','three','four','five','six']
+
+n=list(map(f,l1,l2))
+print(n)
+'''
+
+'''
+//reduce
+from functools import reduce
+def add(x, y) :            # 两数相加
+     return x + y
+
+n=reduce(add, [1,2,3,4,5])   # 计算列表和：1+2+3+4+5
+
+print(n)
+'''
+
+'''
+//filter
+def f1(x):
+    if x > 20:
+        return True
+    else:
+        return False
+
+l1 = [ 1, 2, 3, 42, 67, 16 ]
+f=filter( f1, l1)
+result=list(f)
+print(result)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
